7ed337ab12512627dc344e87cc34a0638e2b2188
wendelsantosd <wendelwcsantos@gmail.com>
2024-01-22 01:53:30 -0300

    chore: remove console.log()

--- src/shared/infra/db/prisma.service.ts
+++ src/shared/infra/db/prisma.service.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    console.log('PRISMA STARTED');
     await this.$connect();
   }
 }
